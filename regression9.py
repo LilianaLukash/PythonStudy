9a8dc3a3d925f5bdc7745c8d5e5baf0f18179aa9
LilianaLukash <liliana.ubisoft@gmail.com>
2022-12-28 18:29:00 +0000

    Fibonacci Recursion

---
+++ regression9.py
@@ -0,0 +1,18 @@
+# create Fibonacci Recursion
+list=[]
+def FibRecursion(rnumb):
+    if rnumb == 1:
+        return 0
+    elif rnumb ==2:
+        return 1
+    else:
+        nfib = FibRecursion(rnumb-1)+FibRecursion(rnumb-2)
+        return nfib
+
+inpR = int(input("Enter integer for Fibonacci Recursion"))
+for i in range(inpR):
+    next= FibRecursion(i+1)
+    list.append(next)
+flist = FibRecursion(inpR)
+print(flist)
+print(list)
