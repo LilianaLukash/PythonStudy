60dd7450240c61cda047685f1e936cff80328e4b
LilianaLukash <liliana.ubisoft@gmail.com>
2023-01-15 00:53:13 +0000

    Home work 11

---
+++ 12/hw3.py
@@ -0,0 +1,12 @@
+class HighlightBlock:
+    def __enter__(self):
+        print("==========")
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("==========")
+        if exc_val is not None:
+            print(f"Error: {exc_val}")
+        return False
+
+with HighlightBlock():
+    print("This is the block of code being highlighted")
+    raise ValueError("An error occurred.")
---
+++ 12/hw4.py
@@ -0,0 +1,7 @@
+try:
+    print("==========")
+
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    print("==========")
